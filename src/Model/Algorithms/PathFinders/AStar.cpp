5636264f4f95b342dd252a27dfb88b7eb393f87c
Pouya <pouyamaleki85@gmail.com>
2026-07-07 18:12:06 +0330

    add filtering system to A* algorithm

--- src/Model/Algorithms/PathFinders/AStar.cpp
+++ src/Model/Algorithms/PathFinders/AStar.cpp
@@ -7,4 +7,42 @@ struct AstarResult
     int totalWeight;
 };
 
-AstarResult AStar(const Graph& graph , char from , char to , char wolfPosition , heuristicFunction heuristic , )
+// A* algorithm function implementation
+AstarResult AStar(const Graph &graph, char from, char to, char wolfPosition, heuristicFunction heuristic)
+{
+    // create a new graph to remove the wolf position
+    Graph newgraph;
+
+    // a for loop to create the nodes in filtered graph
+    for (const auto &node : graph.getAdjList())
+    {
+        if (node.first != wolfPosition)
+        {
+            newgraph.addNode(node.first);
+        }
+    }
+
+    // a for loop to copy the valid edges
+    for (const auto &node : graph.getAdjList())
+    {
+        char fromNode = node.first;
+        // skip the wolf position node
+        if (fromNode == wolfPosition)
+        {
+            continue;
+        }
+
+        // a for loop to navigate the edges of every node
+        for (const auto &edge : node.second)
+        {
+            char toNode = edge.first;
+            int weight = edge.second;
+
+            // skip the edges that connect to wolf position
+            if (to != wolfPosition)
+            {
+                newgraph.addEdge(fromNode, toNode, weight);
+            }
+        }
+    }
+}
