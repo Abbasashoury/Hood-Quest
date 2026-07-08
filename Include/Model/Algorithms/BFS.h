c74acd9f96dfb40b47b2c415af0bb910289fa505
Pouya <pouyamaleki85@gmail.com>
2026-07-09 00:01:31 +0330

    write BFS header file

---
+++ Include/Model/Algorithms/BFS.h
@@ -0,0 +1,13 @@
+#ifndef BFS_H
+#define BFS_H
+
+#include "Graph.h"
+#include <queue>
+#include <algorithm>
+
+// bfs function to find the best path for the wolf
+vector<char> BFS(const Graph &, char, char);
+// return the next postion that the wolf should go
+char wolfNextPosition(char , char);
+
+#endif BFS_H
