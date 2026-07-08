2ab1ee87cbf60c3b632de52b82d67e64dfa533f3
Pouya <pouyamaleki85@gmail.com>
2026-07-09 00:03:57 +0330

    write the basices of the BFS source file

---
+++ src/Model/Algorithms/BFS.cpp
@@ -0,0 +1,10 @@
+#include "BFS.h"
+
+// BFS algorithm implementation
+vector<char> BFS(const Graph &graph, char playerPosition, char wolfPosition)
+{
+
+}
+
+// wolf next postion implementation
+char wolfNextPosition(char , char);
