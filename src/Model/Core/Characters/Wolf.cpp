f13cf73844a2122d2dd90070e05e06c2937f34c7
Pouya <pouyamaleki85@gmail.com>
2026-07-09 15:38:20 +0330

    complete wolf sorce file

--- src/Model/Core/Characters/Wolf.cpp
+++ src/Model/Core/Characters/Wolf.cpp
@@ -1,2 +1,19 @@
 #include "Wolf.h"
 
+// wolf calss constructor implementation
+wolf::wolf(char position)
+{
+    setPosition(position);
+}
+
+// set positoin implementation
+void wolf::setPosition(char position)
+{
+    wolfPosition = position;
+}
+
+// get position method implementation
+char wolf::getPosition() const
+{
+    return wolfPosition;
+}
