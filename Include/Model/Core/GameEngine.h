cf0e9002d188da4790137686c0a788515a571d0d
Abbas Ashoury <abbasashoury84@gmail.com>
2026-07-09 12:56:38 +0330

    add "GameEngine class"

---
+++ Include/Model/Core/GameEngine.h
@@ -0,0 +1,14 @@
+#ifndef GAMEENGINE_H
+#define GAMEENGINE_H
+
+#include "CLIView.h"
+#include "InputHandler.h"
+using namespace std;
+class Gameengine
+{
+private:
+public:
+    void GameLoop();
+};
+
+#endif GAMEENGINE_H
---
+++ src/Model/Core/GameEngine.cpp
@@ -0,0 +1,7 @@
+#include "GameEngine.h"
+
+void Gameengine::GameLoop()
+{
+    Cliview cli;
+    
+}
