2f832217c77ab55b8a88d7746b3329972eab4a20
Abbas Ashoury <abbasashoury84@gmail.com>
2026-07-12 17:16:49 +0330

    add "BSTNode struct" to the "BST class"

---
+++ Include/Model/DataStructure/BST.h
@@ -0,0 +1,24 @@
+#ifndef BST_H
+#define BST_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct BSTNode
+{
+    string username;
+    long int score;
+    BSTNode *left;
+    BSTNode *right;
+
+    BSTNode(const string &u, long int s) : username(u), score(s), left(nullptr), right(nullptr) {}
+};
+
+class BST
+{
+private:
+public:
+};
+
+#endif
